664cb5b5ba72afd8d66ed0ec21bda69d54e60b10
Marcio <m.antonio.cs00@gmail.com git config --global user.email m.antonio.cs00@gmail.com>
2025-04-07 11:50:29 -0300

    Some repairs

--- MauiAppTempo/MainPage.xaml.cs
+++ MauiAppTempo/MainPage.xaml.cs
@@ -1,25 +1,53 @@
-﻿namespace MauiAppTempo
+﻿using MauiAppTempo.Models;
+using MauiAppTempo.Services;
+
+namespace MauiAppTempo
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                    if (t != null)
+                    {
+                        string dadosprev = "";
+                        dadosprev =
+                                    $"Temperatura Mínima: {t.temp_min}°C\n" +
+                                    $"Temperatura Máxima: {t.temp_max}°C\n" +
+                                    $"Descrição: {t.description}\n" +
+                                    $"Velocidade do Vento: {t.speed} m/s\n" +
+                                    $"Visibilidade: {t.visibility} m\n" +
+                                    $"Nascer do Sol: {t.sunrise}\n" +
+                                    $"Pôr do Sol: {t.sunset}\n";
+                    }
+                    else
+                    {
+                        lbl_resultado.Text = "Cidade não encontrada";
+                    } }
+                else
+                {
+                    lbl_resultado.Text = "Digite uma cidade";
+                }
+                 }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            } 
+        
+        
         }
     }
+    }
 
-}
+
